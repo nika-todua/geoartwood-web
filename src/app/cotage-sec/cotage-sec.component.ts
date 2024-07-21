45048017a2e0e59252897ede80c8533235effcad
nika todua <alchimikos@gmail.com>
2024-07-21 15:44:33 +0400

    slider update

--- src/app/cotage-sec/cotage-sec.component.ts
+++ src/app/cotage-sec/cotage-sec.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-cotage-sec',
@@ -6,82 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './cotage-sec.component.scss'
 })
 export class CotageSecComponent {
-  shuffle(array:any) {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex > 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-    return array;
+  ngOnInit(){
+    var splide = new Splide( '.mysplider1', {
+      perPage: 4,
+      gap: '25px',
+      breakpoints: {
+        1200: {
+          perPage: 4,
+          gap    : '25px',
+          height : '500px',
+        },
+        810: {
+          perPage: 3,
+          gap    : '15px',
+          height : '500px',
+        },
+        600: {
+          perPage: 2,
+          gap    : '15px',
+          height : '500px',
+        },
+        480: {
+          perPage: 1,
+          gap    : '12.5px',
+          height : '500px',
+        },
+      },
+    } );
+    splide.mount();
   }
-  
-  
-  ngOnInit(){  
-    this.shuffle(this.caruselarr);
-
-    window.addEventListener("resize", () => {
-      this.caruselviwes()
-    })
-    window.addEventListener("DOMContentLoaded", () => {
-      this.caruselviwes()
-    })
-    
-  }
-  
-  caruselviwes(){
-    if(window.innerWidth >= 480){
-      document.querySelector("swiper-container")!.setAttribute('slides-per-view', '2');
-      document.querySelector("swiper-container")!.setAttribute('space-between', '12.5');
-    }
-    if(window.innerWidth >= 600){
-      document.querySelector("swiper-container")!.setAttribute('slides-per-view', '2');
-      document.querySelector("swiper-container")!.setAttribute('space-between', '15');
-    }
-    if(window.innerWidth >= 810){
-      document.querySelector("swiper-container")!.setAttribute('slides-per-view', '3');
-      document.querySelector("swiper-container")!.setAttribute('space-between', '25');
-    }
-    if(window.innerWidth >= 1200){
-      document.querySelector("swiper-container")!.setAttribute('slides-per-view', '4');
-      document.querySelector("swiper-container")!.setAttribute('space-between', '25');
-    }
-    if(window.innerWidth < 480){
-      document.querySelector("swiper-container")!.setAttribute('slides-per-view', '1');
-      document.querySelector("swiper-container")!.setAttribute('space-between', '12.5');
-    }
-  }
-  
-  caruselarr = [
-    {
-      img_big: '../../../assets/img/kotejebi/koteji1.webp',
-      alt: 'Cottage 1',
-    },
-    {
-      img_big: '../../../assets/img/kotejebi/koteji2.webp',
-      alt: 'Cottage 2',
-    },
-    {
-      img_big: '../../../assets/img/kotejebi/koteji3.webp',
-      alt: 'Cottage 3',
-    },
-    {
-      img_big: '../../../assets/img/kotejebi/koteji4.webp',
-      alt: 'Cottage 4',
-    },
-    {
-      img_big: '../../../assets/img/kotejebi/u4YOdnFmSAW3fykeggTtjvZb8q1Zs5AczOC2MhFB.jpg',
-      alt: 'Cottage 5',
-    },
-    {
-      img_big: '../../../assets/img/kotejebi/koteji5.jpg',
-      alt: 'Cottage 6',
-    },
-    {
-      img_big: '../../../assets/img/kotejebi/koteji6.jpg',
-      alt: 'Cottage 7',
-    }
-  ]
   
 }
 interface OnInit {
--- src/app/sauna/sauna.component.ts
+++ src/app/sauna/sauna.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-sauna',
@@ -6,124 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './sauna.component.scss'
 })
 export class SaunaComponent {
-  shuffle(array:any) {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex > 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-    return array;
-  }
   
-  ngOnInit(){  
-    window.addEventListener("resize", () => {
-      this.caruselviwes()
-    })
-    window.addEventListener("DOMContentLoaded", () => {
-      this.caruselviwes()
-    })
-    
-    this.shuffle(this.saunacaruselarr);
+  ngOnInit(){
+
+    var splide = new Splide( '.mysplider2', {
+      perPage: 4,
+      gap: '25px',
+      breakpoints: {
+        1200: {
+          perPage: 4,
+          gap    : '25px',
+          height : '500px',
+        },
+        810: {
+          perPage: 3,
+          gap    : '15px',
+          height : '500px',
+        },
+        600: {
+          perPage: 2,
+          gap    : '15px',
+          height : '500px',
+        },
+        480: {
+          perPage: 1,
+          gap    : '12.5px',
+          height : '500px',
+        },
+      },
+    } );
+    splide.mount();
   }
 
-  caruselviwes(){
-    if(window.innerWidth >= 480){
-      document.getElementById("saunaslider")!.setAttribute('slides-per-view', '2');
-      document.getElementById("saunaslider")!.setAttribute('space-between', '12.5');
-    }
-    if(window.innerWidth >= 600){
-      document.getElementById("saunaslider")!.setAttribute('slides-per-view', '2');
-      document.getElementById("saunaslider")!.setAttribute('space-between', '15');
-    }
-    if(window.innerWidth >= 810){
-      document.getElementById("saunaslider")!.setAttribute('slides-per-view', '3');
-      document.getElementById("saunaslider")!.setAttribute('space-between', '25');
-    }
-    if(window.innerWidth >= 1200){
-      document.getElementById("saunaslider")!.setAttribute('slides-per-view', '4');
-      document.getElementById("saunaslider")!.setAttribute('space-between', '25');
-    }
-    if(window.innerWidth < 480){
-      document.getElementById("saunaslider")!.setAttribute('slides-per-view', '1');
-      document.getElementById("saunaslider")!.setAttribute('space-between', '12.5');
-    }
-  }
-
-  saunacaruselarr = [
-    {
-      img_big: '../../../assets/img/sauna/sauna1.jpeg',
-      imgalt: 'sauna 1'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna2.jpeg',
-      imgalt: 'sauna 2'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna3.jpeg',
-      imgalt: 'sauna 3'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna4.jpeg',
-      imgalt: 'sauna 4'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna5.jpeg',
-      imgalt: 'sauna 5'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna6.jpeg',
-      imgalt: 'sauna 6'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna7.jpeg',
-      imgalt: 'sauna 7'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna8.jpeg',
-      imgalt: 'sauna 8'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna9.jpeg',
-      imgalt: 'sauna 9'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna10.jpeg',
-      imgalt: 'sauna 10'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna11.jpeg',
-      imgalt: 'sauna 11'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna12.jpeg',
-      imgalt: 'sauna 12'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna13.jpeg',
-      imgalt: 'sauna 13'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna14.jpeg',
-      imgalt: 'sauna 14'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna15.jpeg',
-      imgalt: 'sauna 15'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna16.jpeg',
-      imgalt: 'sauna 16'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna17.jpeg',
-      imgalt: 'sauna 17'
-    },
-    {
-      img_big: '../../../assets/img/sauna/sauna18.jpg',
-      imgalt: 'sauna 18'
-    }
-  ]
 }
 interface OnInit {
   ngOnInit(): void
