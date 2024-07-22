c2872fb2534281a48884de31f250af5db1899813
nika todua <alchimikos@gmail.com>
2024-07-22 18:13:56 +0400

    update

--- src/app/cotage-sec/cotage-sec.component.ts
+++ src/app/cotage-sec/cotage-sec.component.ts
@@ -9,15 +9,16 @@ import Splide from '@splidejs/splide';
 export class CotageSecComponent {
   ngOnInit(){
     var splide = new Splide( '.mysplider1', {
-      perPage: 4,
+      perPage: 5,
       gap: '25px',
+      perMove: 1,
       breakpoints: {
-        1200: {
+        1440: {
           perPage: 4,
           gap    : '25px',
           height : '500px',
         },
-        859: {
+        1115: {
           perPage: 3,
           gap    : '15px',
           height : '500px',
--- src/app/sauna/sauna.component.ts
+++ src/app/sauna/sauna.component.ts
@@ -11,15 +11,16 @@ export class SaunaComponent {
   ngOnInit(){
 
     var splide = new Splide( '.mysplider2', {
-      perPage: 4,
+      perPage: 5,
       gap: '25px',
+      perMove: 1,
       breakpoints: {
-        1200: {
+        1440: {
           perPage: 4,
           gap    : '25px',
           height : '500px',
         },
-        859: {
+        1115: {
           perPage: 3,
           gap    : '15px',
           height : '500px',
